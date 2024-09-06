c7198c8a4d6720f9f4de393b3d83aa6695be1bc9
Emiliano Steinvorth <75647452+Steinvorth@users.noreply.github.com>
2024-09-06 12:43:31 -0600

    Cors and SWAGGER

---
+++ MinimalAPI/Entities/Genre.cs
@@ -0,0 +1,8 @@
+﻿namespace MinimalAPI.Entities
+{
+    public class Genre
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,6 +1,73 @@
+using Microsoft.AspNetCore.Cors;
+using MinimalAPI.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
+var allowedOrigins = builder.Configuration.GetValue<string>("allowed_origins")!;
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy( settings =>
+    {
+        settings.WithOrigins(allowedOrigins) //allow any origin
+               .AllowAnyMethod() //allow any HTTP method POST,PUT,DELETE,GET etc
+               .AllowAnyHeader(); //allow any header to be used to make the request
+    });
+
+    options.AddPolicy("open", settings =>
+    {
+        settings.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+
+});
+
+builder.Services.AddOutputCache();
+//swagger configuration
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if(builder.Environment.IsDevelopment())
+{
+    // swagger ui for development only
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(); //allow app to use the defined cores above.
+app.UseOutputCache(); //use the output cache middleware
+
+app.MapGet("/", [EnableCors(policyName: "open")] () => "Hello World!"); //adding a policy will change how this end point will be accesed.
+
+app.MapGet("/genre", () =>
+{
+    var genres = new List<Genre>
+    {
+        new Genre()
+        {
+            Id = 1,
+            Name = "Action"
+        },
+        new Genre()
+        {
+            Id = 2,
+            Name = "Adventure"
+        },
+        new Genre()
+        {
+            Id = 3,
+            Name = "Comedy"
+        },
+        new Genre()
+        {
+            Id = 4,
+            Name = "Drama"
+        }
+    };
+
+    return genres;
+}).CacheOutput(c => c.Expire(TimeSpan.FromSeconds(15))); //cache the response for 15 seconds
 
 app.Run();
